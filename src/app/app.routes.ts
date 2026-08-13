d7555ddc2d2c30c37a0fb946903e436ded3d5403
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-08-12 21:42:36 -0300

    Inserindo o básico do routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home-component/home-component';
+import { AtletaComponent } from './components/atleta-component/atleta-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:"/home",pathMatch: 'full'},
+  {path:"home", component:HomeComponent},
+  {path:"cadastroAtleta", component:AtletaComponent}
+];
--- src/app/components/menu-component/menu-component.ts
+++ src/app/components/menu-component/menu-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '../../../../node_modules/@angular/router/types/_router_module-chunk';
 
 @Component({
   selector: 'app-menu-component',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-component.html',
   styleUrl: './menu-component.css',
 })

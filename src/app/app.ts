75515ee29d178e7981456a56a18ebceaa2d2102e
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-08-12 21:18:48 -0300

    Criação Báscia

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu-component/menu-component';
+import { AtletaComponent } from './components/atleta-component/atleta-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent,AtletaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
